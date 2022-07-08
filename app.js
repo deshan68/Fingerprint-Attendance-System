910cce728801482b9f056969a640443783d0ca60
deshan68 <arundeshan@gmail.com>
2022-07-08 19:52:37 +0530

    conect to the firebase DB

---
+++ app.js
@@ -0,0 +1,11 @@
+//reference
+var FingerId = document.getElementById("FingerId");
+var UserName = document.getElementById("UserName");
+var SrlNum = document.getElementById("SrlNum");
+var EmlAddrs = document.getElementById("EmlAddrs");
+
+var addFingerBtn = document.getElementById("addFingerBtn");
+var AddUsrBtn = document.getElementById("AddUsrBtn");
+var UpdtUsrBtn = document.getElementById("UpdtUsrBtn");
+var RmvUsrBtn = document.getElementById("RmvUsrBtn");
+
